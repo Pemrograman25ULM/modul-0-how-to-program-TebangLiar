b0e24c1194874e67a3d28f62e422b36775fbfefe
TebangLiar <xireinakun@gmail.com>
2025-09-28 15:11:57 +0800

    Add files via upload

---
+++ C/PRAK002-2510817110004-AhmadRiyadhi.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Selamat Pagi, Ahmad Riyadhi\nSelamat siang, Ahmad Riyadhi\nSelamat Malam, Ahmad Riyadhi\n");
+    getchar(); // Waits for user input 
+    return 0;
+}
